4c413c6be105d53a3b6ff87b0ed13ba3b4e620e2
mading8080 <madjidsafari@gmail.com>
2024-12-19 08:36:54 +0100

    Ecraser le dépôt distant avec le contenu local

--- gestion_production/production/urls.py
+++
@@ -1,7 +0,0 @@
-# production/urls.py
-from django.urls import path
-from . import views  # Assurez-vous d'importer vos vues
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Route pour la page d'accueil
-]
--- gestion_production/production/views.py
+++
@@ -1,5 +0,0 @@
-# production/views.py
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'production/dashboard.html')  # Assurez-vous que le template existe
